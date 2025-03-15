be390009363643bbebe450360ead53a69a1e49a7
Valentin Latunov <vlatunov@gmail.com>
2025-03-15 19:48:38 +0200

    Initial project setup
    Set up an empty project.

---
+++ cmake/compiler_options.cmake
@@ -0,0 +1,4 @@
+function(set_compiler_options)
+	set(CXX_STANDARD 17)
+	set(CXX_STANDARD_REQUIRED TRUE)
+endfunction()
---
+++ cmake/setup_project.cmake
@@ -0,0 +1,5 @@
+include(cmake/compiler_options.cmake)
+
+function(setup_project)
+	set_compiler_options()
+endfunction()
